b18426ba8156e132b228ab5387e95247a3a52cba
DonovanYe <xxiaozhiyzj@gmail.com>
2025-09-03 11:20:31 +0800

    chore: show debug log in prod

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,7 +2,8 @@ import { createConsola, LogLevels } from "consola";
 import { IS_DEV } from "./const";
 
 const logger = createConsola({
-  level: IS_DEV ? LogLevels.debug : LogLevels.info,
+  // level: IS_DEV ? LogLevels.debug : LogLevels.info,
+  level: LogLevels.debug,
   defaults: {
     tag: "better-chatbot",
   },
